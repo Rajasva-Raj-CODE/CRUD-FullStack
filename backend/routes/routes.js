027ef2f517a9864ce0a338e18d06e9b2170b02ad
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-11-04 23:05:15 +0530

    put api done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import {Createuser,Getuser} from '../controller/userController.js'
+import {Createuser,Getuser,Updateuser} from '../controller/userController.js'
 
 const routers = express.Router()
 
 routers.post('/create',Createuser)
 routers.get('/get',Getuser)
-
+routers.put('/update/:id',Updateuser)
 
 export default routers
